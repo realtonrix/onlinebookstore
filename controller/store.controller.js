63d6cd235828d48b77e12f51aebb8116626c4bc9
realtonrix <realtonrix@gmail.com>
2025-05-09 18:36:17 +0300

    Modified Book Controller.

--- controller/store.controller.js
+++ controller/store.controller.js
@@ -1,12 +1,12 @@
 const storeModel = require("../model/store.model");
-const cloudinary = require("../config/cloudinary");
+const cloudinery = require("../config/cloudinary");
 const userModel = require("../model/user.model");
 const mongoose = require("mongoose");
 
 const createBook = async (req, res) => {
   try {
     const { title, author, summary } = req.body;
-    const cloudImage = await cloudinary.uploader.upload(req.file.path);
+    const cloudImage = await cloudinery.uploader.upload(req.file.path);
     const getUser = await userModel.findById(req.params.id);
     const isbnNumberCreate = Math.floor(Math.random() * 10000);
 
@@ -30,7 +30,7 @@ const createBook = async (req, res) => {
       data: createBook,
     });
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     res.status(400).json({
       message: "Failed to create new book",
       data: error,
@@ -38,77 +38,16 @@ const createBook = async (req, res) => {
   }
 };
 
-const getBook = async(req, res) => {
+const getBook = async (req, res) => {
   try {
     const getUser = await userModel.findById(req.params.id).populate("shop");
     res.status(200).json({
-      message: "Your books",
+      message: "Your Books",
       data: getUser,
     });
-
-  }
-  catch (error){
-    res.status(400).json({
-      message: "Failed to get book",
-      data: error,
-    });
-  }
-};
-
-const getSingleBook = async(req, res) =>{
-  try{
-    const bookData = await storeModel.findByID(req.params.id);
-    res.status(200).json({
-      message: "Single Book gotten successfully",
-      data: bookData,
-    });
-  
-  }
-  catch(error){
-    res.status(400).json({
-      message: "Failed to get a single book",
-      data: error,
-    });
-  }
-};
-
-const deleteBook = async (req, res) =>{
-  try{
-    const geUser = await userModel.findByID(req.params.id);
-    const removeBook = await storeModel.findByIdAndDelete(req.params.bookID);
-
-    getUser.shop.pull(removeBook);
-    getUser.save();
-
-    res.status(200).json({
-      message: "deleted successfully",
-      data: getUser,
-    });
-
-  } catch (error){
-    res.status(400).json({
-      message: "Failed to delete book",
-      data: error,
-    });
-  }
-
-};
-
-const updateBook = async (req, res) =>{
-  try {
-      const {title, author} = req.body;
-      const updateData = await storeModel.findByIdAndUpdate(req.params.id, 
-        {title, author}, 
-        {new: true}
-      );
-     
-      res.status(201).json({
-        message: "Updated successfully",
-        data: updateData,
-      });
   } catch (error) {
     res.status(400).json({
-      message: "Failed to update the book",
+      message: "Failed to get book",
       data: error,
     });
   }
@@ -129,4 +68,67 @@ const getAllBook = async (req, res) => {
   }
 };
 
-module.exports = { createBook, getBook, getSingleBook, deleteBook, updateBook, getAllBook};
+const getSingleBook = async (req, res) => {
+  try {
+    const bookData = await storeModel.findById(req.params.id);
+    res.status(200).json({
+      message: "Single book gotten",
+      data: bookData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to get single book",
+      data: error,
+    });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const getUser = await userModel.findById(req.params.id);
+    const removeBook = await storeModel.findByIdAndDelete(req.params.bookID);
+
+    getUser.shop.pull(removeBook);
+    getUser.save();
+
+    res.status(200).json({
+      message: "Deleted Successfully",
+      data: getUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to delete book",
+      data: error,
+    });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const { title, author } = req.body;
+    const updateData = await storeModel.findByIdAndUpdate(
+      req.params.id,
+      { title, author },
+      { new: true }
+    );
+    res.status(201).json({
+      message: "Updated Successfully",
+      data: updateData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "Failed to Update book",
+      data: error,
+    });
+  }
+};
+
+module.exports = {
+  createBook,
+  getBook,
+  getSingleBook,
+  deleteBook,
+  updateBook,
+  getAllBook,
+};
